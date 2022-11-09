cdbeb65fcfa97287a6256fc921dbb8482a24e675
DevonMilley <dmilley@terpmail.umd.edu>
2022-11-09 13:42:03 -0500

    finish lab

--- chart-js-lab/line-chart/javascript.js
+++ chart-js-lab/line-chart/javascript.js
@@ -7,37 +7,27 @@ var myChart = new Chart(ctx, {
         datasets: [{
             label: 'Brazil',
             data: [32, 32, 31.99, 32.06, 32.4, 32.75, 51.78, 63.65, 71.44, 76.33],
-            fill: true,
             borderColor: 'rgba(70, 66, 255, 0.8)',
-            backgroundColor: 'rgba(70, 66, 255, 0.1)',
             tension: 0.1},
         {
             label: 'China',
             data: [32, 32, 32,31.96, 31.75, 31.55, 40.73, 63.26, 70.03, 77.4],
-            fill: true,
             borderColor: 'rgba(7, 187, 213, 0.8)',
-            backgroundColor: 'rgba(7, 187, 213, 0.1)',
             tension: 0.1},
         {
             label: 'India',
             data: [25.44, 25.44, 25.44, 25.11, 18.37, 26.69, 34.92, 51.51, 62.34, 69.77],
-            fill: true,
             borderColor: 'rgba(51, 164, 252, 0.8)',
-            backgroundColor: 'rgba(51, 164, 252, 0.1)',
             tension: 0.1},
         {
             label: 'Indonesia',
             data: [30, 30, 30, 30.04, 30.24, 30.43, 37.16, 56.49, 67.27, 72.51],
-            fill: true,
             borderColor: 'rgba(7, 213, 195, 0.8)',
-            backgroundColor: 'rgba(7, 213, 195, 0.1)',
             tension: 0.1},
         {
             label: 'United States',
             data: [39.41, 39.41, 39.41, 39.41, 48.92, 58.47, 68.07, 72.44, 76.9, 79.59],
-            fill: true,
             borderColor: 'rgba(84, 7, 213, 0.8)',
-            backgroundColor: 'rgba(84, 7, 213, 0.1)',
             tension: 0.1}
         ]
     },
--- chart-js-lab/scatter-plot/javascript.js
+++ chart-js-lab/scatter-plot/javascript.js
@@ -29,21 +29,89 @@ var ctx = document.getElementById('myChart').getContext('2d');
                 backgroundColor:"rgb(255,165,0,0.1)",
                 borderWidth:2,
                 
+              }, { 
+                data: [
+                {x: 68.3, y: 5878},
+                ],
+                label: "Indonesia",
+                borderColor: "rgba(84, 7, 213, 0.8)",
+                backgroundColor:"rgba(84, 7, 213, 0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                {x: 71.9, y: 11461},
+                ],
+                label: "Brazil",
+                borderColor: "rgba(59, 237, 207, 0.8)",
+                backgroundColor:"rgba(59, 237, 207, 0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                {x: 65.4, y: 13173},
+                ],
+                label: "Russia",
+                borderColor: "rgba(216, 0, 135, 0.8)",
+                backgroundColor:"rgba(216, 0, 135, 0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                {x: 62.6, y: 3366},
+                ],
+                label: "Pakistan",
+                borderColor: "rgba(216, 0, 0, 0.8)",
+                backgroundColor:"rgba(216, 0, 0, 0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                {x: 65.8, y: 1632},
+                ],
+                label: "Bangladesh",
+                borderColor: "rgba(170, 36, 237, 0.8)",
+                backgroundColor:"rgba(170, 36, 237, 0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                {x: 81.1, y: 32193},
+                ],
+                label: "Japan",
+                borderColor: "rgba(204, 237, 36, 0.8)",
+                backgroundColor:"rgba(204, 237, 36, 0.1)",
+                borderWidth:2,
+                
               }
             ]
           },
           options: {
+            responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Top 10 countries\' life expectancy and GDP per capita',
+                font: {
+                    size: 18
+                }
+            },
+            subtitle: {
+                display: true,
+                text: 'The United States has the higest GDP per capita but only the second highest life expectancy. There is a positive correlation between life expectanct and GDP per capita.'
+            }
+        },
             scales: {
               y: {
                 title: {
                   display: true,
-                  text: 'Y axis title'
+                  text: 'GDP per capita'
                 }
               },
               x: {
                   title: {
                       display: true,
-                      text: 'X axis title'
+                      text: 'Life expectancy'
                   }
               }
             }
