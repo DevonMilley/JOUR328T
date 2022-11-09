fcc9074d7e8f836d82058d7beee381a2c7151907
DevonMilley <dmilley@terpmail.umd.edu>
2022-11-09 12:34:52 -0500

    line chart stuff

--- chart-js-lab/line-chart/javascript.js
+++ chart-js-lab/line-chart/javascript.js
@@ -1,40 +1,46 @@
 var ctx = document.getElementById('myChart').getContext('2d');
-
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
+
     data: {
-        labels: ["America", "East Asia & Pacific", "Europe & Central Asia", "Middle East & North Africa", "South Asia",
-        "Sub-Saharan Africa"],
+        labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
         datasets: [{
-            label: 'GDP per capita',
-            data: [18791.13, 14480.30, 20818.45, 13856.00, 2505.17, 2569.00],
-            backgroundColor: [
-                '#ff0800'
-            ]
-        },
-    ]
+            label: '# of Apples',
+            data: [12, 19, 3, 5, 2, 3],
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+        {
+            label: '# of Oranges',
+            data: [22, 10, 23, 6, 22, 13],
+            fill: true,
+            borderColor: 'rgba(255, 165, 0)',
+            backgroundColor: 'rgba(255, 165, 0, 0.1)',
+            tension: 0.1}
+        ]
     },
     options: {
         responsive: true,
         plugins: {
             title: {
                 display: true,
-                text: 'GDP per capita by region',
+                text: 'Apples and Oranges by year',
                 font: {
                     size: 18
                 }
             },
             subtitle: {
                 display: true,
-                text: 'Europe and Central Asia have the highest GDP per capita'
+                text: 'For all but one year, oranges outnumbered apples'
             }
         },
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
             }
         },
     },
-});  
+});
  
 
