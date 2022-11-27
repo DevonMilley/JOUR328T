f7dbb3aa2f3ec231256d686308ff7957af32e9d4
DevonMilley <dmilley@terpmail.umd.edu>
2022-11-27 12:01:09 -0500

    scatter plot

---
+++ chart-js-homework/scatter-plot/javascript.js
@@ -0,0 +1,481 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+      var myChart = new Chart(ctx, {
+          type: 'scatter',
+          data: {
+            datasets: [{ 
+                data: [
+                  {x: 20.7, y: 19.36},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+               
+              }, { 
+                data: [
+                  {x: 10.47, y: 11.01},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                {x: 20.27, y: 19.29},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor:"rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 24.21, y: 29.91},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 3.66, y: 10.37},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 12.18, y: 15.62},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 39.34, y: 26.7},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 10.13, y: 10.57},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 24, y: 15.79},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 27.65, y: 25.12},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 9.89, y: 2.84},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 17.41, y: 9.02},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 17.45, y: 15.95},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 6.82, y: 80.98},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 8.89, y: 14.18},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 3.26, y: 19.14},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 19.26, y: 14.93},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 17.76, y: 17.37},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 18.99, y: 23.49},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 19.53, y: 14.04},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 18.77, y: 27.35},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 2.15, y: 3.8},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 23.63, y: 27.42},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 24.2, y: 32.87},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 9, y: 11.92},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 26.86, y: 41.06},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 10.16, y: 12.65},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 6.65, y: 28.14},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 15.47, y: 11.31},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 2.34, y: 16.49},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 7.49, y: 8.72},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 5.39, y: 10.32},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 33.74, y: 28.66},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 5.98, y: 10.87},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 5.42, y: 20.84},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 22.6, y: 26.08},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 12.81, y: 20.86},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 2, y: 10.26},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 3.48, y: 5.67},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 6.61, y: 11.36},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 40.13, y: 38.02},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 13.24, y: 39.1},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 21.69, y: 27.36},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 25.96, y: 17.17},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 19.12, y: 24.12},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 44.97, y: 30.48},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 35.05, y: 27.99},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 0.83, y: 6.09},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 30.28, y: 21.41},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 31.63, y: 19.42},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 35.78, y: 33.94},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 20.51, y: 21.15},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 42.98, y: 30.94},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 22.57, y: 37.8},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }, { 
+                data: [
+                  {x: 21.82, y: 31.61},
+                ],
+                borderColor: "#3e95cd",
+                backgroundColor: "rgb(62,149,205,0.1)",
+                borderWidth:2,
+                
+              }
+            ]
+          },
+          options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: 'In Baltimore neighborhoods with more poverty, there is more crime',
+                    font: {
+                        size: 18
+                    }
+                },
+                subtitle: {
+                    display: true,
+                    text: 'The relationship between crime and poverty in Baltimore neighborhoods'
+                }
+            },
+            scales: {
+              y: {
+                title: {
+                  display: true,
+                  text: 'Crime'
+                }
+              },
+              x: {
+                  title: {
+                      display: true,
+                      text: 'Poverty'
+                  }
+              }
+            }
+          }
+        });
